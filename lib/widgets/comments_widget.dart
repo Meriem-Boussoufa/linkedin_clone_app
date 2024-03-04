72e7caf4cd8fdf97810ec9cf735a3cc1696c15a3
meriem <meriem@gmail.com>
2024-03-04 14:12:15 +0100

    add comment widget

---
+++ lib/widgets/comments_widget.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:linkedin_clone_app/search/profile_company.dart';
+
+class CommentWidget extends StatefulWidget {
+  final String commentId;
+  final String commenterId;
+  final String commenterName;
+  final String commentBody;
+  final String commenterimageUrl;
+
+  const CommentWidget(
+      {super.key,
+      required this.commentId,
+      required this.commenterId,
+      required this.commenterName,
+      required this.commentBody,
+      required this.commenterimageUrl});
+
+  @override
+  State<CommentWidget> createState() => _CommentWidgetState();
+}
+
+class _CommentWidgetState extends State<CommentWidget> {
+  final List<Color> _colors = [
+    Colors.amber,
+    Colors.orange,
+    Colors.pink.shade200,
+    Colors.brown,
+    Colors.cyan,
+    Colors.blue,
+    Colors.deepOrange,
+  ];
+  @override
+  Widget build(BuildContext context) {
+    _colors.shuffle();
+    return InkWell(
+      onTap: () {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) =>
+                    ProfileCompanyScreen(userID: widget.commentId)));
+      },
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Flexible(
+              flex: 1,
+              child: Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: _colors[1]),
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: NetworkImage(widget.commenterimageUrl),
+                        fit: BoxFit.fill)),
+              )),
+          const SizedBox(width: 6),
+          Flexible(
+              flex: 5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.commenterName,
+                    style: const TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                        fontSize: 16),
+                  ),
+                  Text(
+                    widget.commentBody,
+                    maxLines: 5,
+                    style: const TextStyle(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.grey,
+                        fontSize: 13),
+                  ),
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
