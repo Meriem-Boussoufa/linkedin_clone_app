b19c6d0b2a343e5e272b789269012dfce05fcf33
meriem <meriem@gmail.com>
2024-03-04 11:41:52 +0100

    checking if the user is logged in or not

--- lib/user_state.dart
+++ lib/user_state.dart
@@ -1,10 +1,42 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:linkedin_clone_app/auth/login.dart';
+import 'package:linkedin_clone_app/jobs/jobs_screen.dart';
 
 class UserState extends StatelessWidget {
   const UserState({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.data == null) {
+            log('User is not logged in yet');
+            return const LoginScreen();
+          } else if (userSnapshot.hasData) {
+            log('user is already logged in');
+            return const JobsScreen();
+          } else if (userSnapshot.hasError) {
+            return const Scaffold(
+              body: Center(
+                child: Text('An Error has been occured. Try again later'),
+              ),
+            );
+          } else if (userSnapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          return const Scaffold(
+            body: Center(
+              child: Text('Something went wrong'),
+            ),
+          );
+        });
   }
 }
