852dd9d84f673454858695aa8064e4355b18ec00
meriem <meriem@gmail.com>
2024-03-04 11:34:48 +0100

    checking if the app is being initialised

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+import 'user_state.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'LinkedIn Clone',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("LinkedIn"),
-        ),
-      ),
-    );
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(
+                body: Center(child: Text('App in being initialized')),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return const MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(
+                body: Center(child: Text('An Error has beeen occured')),
+              ),
+            );
+          }
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'LinkedIn Clone',
+            theme: ThemeData(
+              scaffoldBackgroundColor: Colors.black,
+              primarySwatch: Colors.blue,
+            ),
+            home: const UserState(),
+          );
+        });
   }
 }
