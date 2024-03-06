5dc5b6977c498f5d0ea6d4e40661a7552355a278
meriem <meriem@gmail.com>
2024-03-06 11:26:55 +0100

    fix the upload job function

--- lib/jobs/jobs_screen.dart
+++ lib/jobs/jobs_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:linkedin_clone_app/services/global_variables.dart';
 
 class JobsScreen extends StatefulWidget {
   const JobsScreen({super.key});
@@ -8,6 +11,26 @@ class JobsScreen extends StatefulWidget {
 }
 
 class _JobsScreenState extends State<JobsScreen> {
+  String? jobCategoryFilter;
+
+  void getMyData() async {
+    final DocumentSnapshot userDoc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+    setState(() {
+      name = userDoc.get('name');
+      userImage = userDoc.get('userImage');
+      location = userDoc.get('location');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getMyData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
