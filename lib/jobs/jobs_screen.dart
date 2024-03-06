7b71fb249546829525876dafda72594cabd5c217
meriem <meriem@gmail.com>
2024-03-06 15:34:49 +0100

    showing jobs to users

--- lib/jobs/jobs_screen.dart
+++ lib/jobs/jobs_screen.dart
@@ -2,6 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:linkedin_clone_app/services/global_variables.dart';
+import 'package:linkedin_clone_app/widgets/bottomNavBar.dart';
+import 'package:linkedin_clone_app/widgets/job_widget.dart';
+
+import '../search/search_jobs.dart';
 
 class JobsScreen extends StatefulWidget {
   const JobsScreen({super.key});
@@ -33,6 +37,73 @@ class _JobsScreenState extends State<JobsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        bottomNavigationBar: BottomNavigationBarForApp(indexNum: 0),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.filter_list_outlined),
+            color: Colors.grey,
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SearchScreen()));
+              },
+              icon: const Icon(Icons.search_outlined),
+              color: Colors.grey,
+            )
+          ],
+        ),
+        body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: FirebaseFirestore.instance
+              .collection('jobs')
+              .where('jobCategory', isEqualTo: jobCategoryFilter)
+              .where('recruitment', isEqualTo: true)
+              .orderBy('createdAt', descending: false)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.data?.docs.isNotEmpty == true) {
+                return ListView.builder(
+                    itemCount: snapshot.data?.docs.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return JobWidget(
+                        taskTitle: snapshot.data?.docs[index]['jobTitle'],
+                        taskId: snapshot.data?.docs[index]['jobDescription'],
+                        uploadedBy: snapshot.data?.docs[index]['uploadedBy'],
+                        userImage: snapshot.data?.docs[index]['userImage'],
+                        name: snapshot.data?.docs[index]['name'],
+                        recruitment: snapshot.data?.docs[index]['recruitment'],
+                        email: snapshot.data?.docs[index]['email'],
+                        location: snapshot.data?.docs[index]['location'],
+                        taskDescription: snapshot.data?.docs[index]
+                            ['jobDescription'],
+                      );
+                    });
+              } else {
+                return const Center(
+                  child: Text('There is no jobs.'),
+                );
+              }
+            }
+            return const Center(
+              child: Text(
+                'Something went wrong ',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+            );
+          },
+        ));
   }
 }
